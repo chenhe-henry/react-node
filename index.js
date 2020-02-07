b5c9feef562142e0da698dd365154bf41381dfef
chenhe-henry <chenhe.elec@gmail.com>
2020-02-07 18:22:18 +1100

    get google login/logout

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -13,6 +15,17 @@ mongoose
   .catch(err => console.log(err));
 
 const app = express();
+
+app.use(
+  cookieSession({
+    // maxAge is how long this cookie can exist inside the browser before it is automatically expired for us
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
