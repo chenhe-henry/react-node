c035472891691747fc933553ac50d70971c42157
chenhe-henry <chenhe.elec@gmail.com>
2020-02-10 22:55:35 +1100

    finish google OAuth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-const Header = () => <h2>Header</h2>;
+import Header from "./Header";
+import Landing from "./Landing";
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Route exact path="/" component={Landing}></Route>
-          <Route exact path="/surveys" component={Dashboard}></Route>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing}></Route>
+            <Route exact path="/surveys" component={Dashboard}></Route>
+            <Route exact path="/surveys/new" component={SurveyNew}></Route>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
-export default App;
+export default connect(null, actions)(App);
---
+++ client/src/components/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+class Header extends React.Component {
+  state = {};
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to={this.props.auth ? "/surveys" : "/"} className="brand-logo">
+            Emaily
+          </Link>
+
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+export default connect(mapStateToProps)(Header);
