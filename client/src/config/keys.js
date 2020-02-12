868d4b5af11e579a3bc0db062a164dc5cee4dceb
chenhe-henry <chenhe.elec@gmail.com>
2020-02-12 21:15:28 +1100

    hide api in prod env

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,7 +1,8 @@
 //keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === "production") {
   // we are in production - return the prod set of keys
-  module.exports = require("./prod");
+  const domainApiKey = process.env.REACT_APP_CLIENT_API_KEY;
+  module.exports = domainApiKey;
 } else {
   // we are in development -return the dev keys
   module.exports = require("./dev");
