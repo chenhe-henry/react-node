835f5a6a968cf078ecc1033bfda8897990e91ede
chenhe-henry <chenhe.elec@gmail.com>
2020-02-13 20:50:17 +1100

    update envrc

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -3,8 +3,7 @@ if (process.env.NODE_ENV === "production") {
   // we are in production - return the prod set of keys
 
   //  module.exports = require("./prod");
-  const DOMAIN_API_KEY = process.env.REACT_APP_CLIENT_API_KEY;
-  module.exports = { domainApiKey: DOMAIN_API_KEY };
+  module.exports = { domainApiKey: process.env.REACT_APP_CLIENT_API_KEY };
 } else {
   // we are in development -return the dev keys
   module.exports = require("./dev");
